e6ae9a827b2c09c3d658c5eaca7d5e2f67dd45ac
Larejun <larejun.pineda@flexisourceit.com.au>
2022-11-03 08:46:30 +0800

    Initial markup for Gallery & Home page

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,17 +1,56 @@
+import { useEffect, useState } from 'react'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
-import BaseLayout, { siteTitle } from '../components/layout/base-layout'
+import BaseLayout, { siteTitle } from '@components/layout/base-layout'
+import { getPhotos } from '@lib/photo.service';
+import Photos from '@components/photo/photos';
+import PageNav from '@components/nav/page-nav'
 
-export default function Gallery() {
+interface GalleryProps {
+  data?: Photo[];
+  page: number;
+}
+
+const Gallery = ({ data, page }: GalleryProps) => {
+  const [photos, setPhotos] = useState(data)
+
+  useEffect(() => {
+    console.log('Pager:', page, data)
+    setPhotos(data);
+  }, [page, data])
+
   return (
     <BaseLayout>
       <Head>
-        <title>{siteTitle} - The Gallery</title>
+        <title>{`${siteTitle} - The Gallery`}</title>
       </Head>
 
-      <div className="content-wrap">
-        The gallery
+      <div className="content-wrap">        
+        <Photos data={photos ?? []} />
+        <PageNav page={page} />
       </div>
     </BaseLayout>
   )
+}
+
+export default Gallery
+
+export const getServerSideProps: GetServerSideProps<GalleryProps> = async ({query}) => {
+  const page = Number(query.page) || 1
+  const data = await getPhotos(page)
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      data,
+      page,
+    },
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css'
 
-import BaseLayout from '../components/layout/base-layout'
+import BaseLayout from '@components/layout/base-layout'
 
 export default function Home() {
   return (
     <BaseLayout>
       <div className="content-wrap">
-        <h1 className={styles.title}>Featured Items</h1>
+        <h1 className={styles.title}>Welcome</h1>
       </div>
     </BaseLayout>
   )
